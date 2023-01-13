4ca5d69f48c91b91fd962a0b2c96cf2e8733c23e
Seongmin <chr92@naver.com>
2023-01-13 20:05:38 +0900

    Section4-fixing-errors

--- response-check/ResponseCheck.vue
+++ response-check/ResponseCheck.vue
@@ -1,78 +1,77 @@
 <template>
     <div>
-        <div id="screen" :class="state" @click="onClickScreen">{{ message }}</div>
+        <h1>왜에애애아죠?</h1>
+        <div id="screen" :class="state" @click="onClickScreen">{{message}}</div>
         <template v-if="result.length">
-            <div> 평균 시간 : {{ average }} ms </div>
-            <button @click="onReset"> 리셋 </button>
+            <div>평균 시간: {{average}}ms</div>
+            <button @click="onReset">리셋</button>
         </template>
     </div>
 </template>
 
 <script>
-    let startTime = 0;
-    let endTime = 0;
-    let timeout = null;
+let startTime = 0;
+let endTime = 0;
+let timeout = null;
 
-    export default {
-        data() {
-            return {
-                result: [],
-                state: 'waiting',
-                message: '클릭하면 시작합니다.',
-            }
+export default {
+    data() {
+        return {
+            result: [],
+            state: 'waiting',
+            message: '클릭해서 시작하세요.',
+        };
+    },
+    computed: {
+        average() {
+            return this.result.reduce((a, c) => a + c, 0) / this.result.length || 0;
+        }
+    },
+    methods: {
+        onReset() {
+            this.result = [];
         },
-        computed: {
-            average() {
-                return this.result.reduce((a, c) => a + c, 0) / this.result.length || 0;
-            }
-        },
-        methods: {
-            onReset() {
-                this.result = [];
-            },
+        onClickScreen() {
+            if (this.state === 'waiting') {
+                this.state = 'ready';
+                this.message = '초록색이 되면 클릭하세요.';
+                timeout = setTimeout(() => {
+                    this.state = 'now';
+                    this.message = '지금 클릭!';
+                    startTime = new Date();
+                }, Math.floor(Math.random() * 1000) + 2000); // 2~3초
 
-            onClickScreen() {
-                if ( this.state === 'waiting' ) { // 대기 중일 때
-                    this.state = 'ready';
-                    this.message = '초록색이 되면 클릭하세요.'
+            } else if (this.state === 'ready') {
+                clearTimeout(timeout);
+                this.state = 'waiting';
+                this.message = '너무 성급하시군요! 초록색이 된 후에 클릭하세요.'
 
-                    timeout = setTimeout( () => {
-                        this.state = 'now';
-                        this.message = '지금 클릭하세요.'
-                        startTime = new Date();
-                    }, Math.floor(Math.random() * 1000) + 2000 ); // 2~3초
-
-                } else if ( this.state === 'ready' ) { // 준비 중일 때
-                    clearTimeout(timeout);
-                    this.state = 'waiting';
-                    this.message = '아직 대기중입니다. 다시 시작하려면 클릭하세요.';
-
-                } else if ( this.state === 'now' ) { // 진행 중일 때
-                    endTime = new Date();
-                    this.state = 'waiting'
-                    this.message = '클릭하면 다시 시작합니다.'
-                    this.result.push(endTime - startTime);
-                }
+            } else if (this.state === 'now') {
+                endTime = new Date();
+                this.state = 'waiting';
+                this.message = '클릭해서 시작하세요.';
+                this.result.push(endTime - startTime);
             }
         }
-    };
+    },
+};
 </script>
 
 <style scoped>
-    #screen {
-        width: 300px;
-        height: 200px;
-        text-align: center;
-        user-select: none;
-    }
-    #screen.waiting {
-        background-color: aqua;
-    }
-    #screen.ready {
-        background-color: red;
-        color: white;
-    }
-    #screen.now {
-        background-color: greenyellow;
-    }
+#screen {
+    width: 300px;
+    height: 200px;
+    text-align: center;
+    user-select: none;
+}
+#screen.waiting {
+    background-color: aqua;
+}
+#screen.ready {
+    background-color: red;
+    color: white;
+}
+#screen.now {
+    background-color: greenyellow;
+}
 </style>
